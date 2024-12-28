6a0d2361832bd2de3240676298bfd023eeabc911
AlhassenSabeeh <alhassensabeeh22@gmail.com>
2024-12-28 15:16:02 +0300

    A module for converting a binary string to a decimal number.

---
+++ solutions/binary_to_decimal.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for converting a binary string to a decimal number.
+
+Module contents:
+    - binary_to_decimal: generates the decimal representation of a binary string.
+
+Created on Dec 28, 2024. 
+@author: AL-HASSEN SABEEH
+"""
+
+def binary_to_decimal(binary : str) -> int:
+    """Converts a binary string to its decimal representation.
+
+    Parameters:
+        binary: string, containing only '0' or '1' characters
+
+    Returns -> int, represent the decimal value of a binary string
+
+    Raises:
+        AssertionError: if the argument is not an string
+        AssertionError: if it contain characters other than 'o' or '1'
+
+    >>> binary_to_decimal('100')
+    4
+
+    >>> binary_to_decimal('1000')
+    8
+
+    >>> binary_to_decimal('111')
+    7
+    
+    >>> binary_to_decimal('1010101010101010101010')
+    2796202
+    
+    """
+    # The binary input should be a string containing only '0' or '1' characters
+    assert isinstance(binary, str), "Binary is not an string"
+    assert set(binary) <= {'0', '1'}, "AssertionError: String contains invalid characters"
+    # The strategy recursively converts a binary string to its decimal value
+    # by processing each digit and reducing the string until empty
+    if len(binary) ==0:
+        # Base Case,  f(ϵ)=0
+        return 0
+    # Recursive Case, f(b1b2…bn) = b1 . 2^(n−1) + f(b2b3…b<n-1>)
+    #      /Return Step/                      /Recursive Case/   /Reduction Step/
+    return int(binary[0])*2**(len(binary)-1) +  binary_to_decimal(binary[1:])
---
+++ solutions/tests/test_binary_to_decimal.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Test module for binary_to_decimal function. 
+Includes deliberately faulty tests for debugging practice. 
+
+Test categories: 
+    - Edge Cases: '0', '1' and a large string.
+    - Standard Cases: A collection of different binary strings.
+    - Defensive Tests: wrong input types, assertions. 
+
+Created on Dec 28, 2024.
+
+@author: AL-HASSEN SABEEH
+"""
+import unittest
+
+from ..binary_to_decimal import binary_to_decimal
+
+class TestBinaryToDecimal(unittest.TestCase):
+    """Test the binary_to_decimal function"""
+
+    #Test Edge Cases
+    def test_binary_0(self):
+        """It should evaluate '0' to 0"""
+        self.assertEqual(binary_to_decimal('0'), 0)
+
+    def test_binary_1(self):
+        """It should evaluate '1' to 1"""
+        self.assertEqual(binary_to_decimal('1'), 1)
+
+    #Test Standard Cases
+    def test_binary_10(self):
+        """It should evaluate '10' to 2"""
+        self.assertEqual(binary_to_decimal('10'), 2)
+
+    def test_binary_111(self):
+        """It should evaluate '111' to 7"""
+        self.assertEqual(binary_to_decimal('111'), 7)
+
+    def test_binary_1010(self):
+        """It should evaluate '1010' to 10"""
+        self.assertEqual(binary_to_decimal('1010'), 10)
+
+    def test_binary_1111111111(self):
+        """It should evaluate '1111111111' to 1023"""
+        self.assertEqual(binary_to_decimal('1111111111'), 1023)
+
+    def test_binary_1010101010101010101010(self):
+        """It should evaluate '1010101010101010101010' to 1023"""
+        self.assertEqual(binary_to_decimal('1010101010101010101010'), 2796202)
+
+    #Test Defensive Assertions
+    def test_defensive_check_binary_is_not_string(self):
+        """it should raise an error if the input is not string"""
+        with self.assertRaises(AssertionError):
+            binary_to_decimal(6)
+
+    def test_defensive_check_for_string_not_0_or_1(self):
+        """it should raise an error if the input has char not 0 or 1"""
+        with self.assertRaises(AssertionError):
+            binary_to_decimal('11180101')
+
+    def test_none_input(self):
+        """It should raise AssertionError for None input"""
+        with self.assertRaises(AssertionError):
+            binary_to_decimal(None)

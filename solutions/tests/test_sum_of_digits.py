9ee3c755d55b6eaa03e0e6fd14309ce5cbd89897
Noorelsalam <noorelsalamabdo@gmail.com>
2024-12-28 11:18:07 +0100

    Added the first test

---
+++ solutions/tests/test_sum_of_digits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for testing the sum_of_digits function
+
+Created on 25 Dec 2024
+@author: Noorelsalam Almakki
+"""
+
+import unittest
+from ..sum_of_digits import sum_of_digits
+
+class TestSumOfDigits(unittest.TestCase):
+  """A class for testing the sum_of_digits function"""
+  
+  def test_2_digits(self):
+    """Test the sum_of_digits function with a 2-digit number"""
+    self.assertEqual(sum_of_digits(12), 3)

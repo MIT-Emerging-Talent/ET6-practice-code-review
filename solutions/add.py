968b37cc5086887a3f6e42d3828dd8c2b73b44c1
Ridwan Ayinde <ridwanbabatundeayinde@gmail.com>
2024-12-28 19:03:24 +0100

    Added add.py function and test_add.py

--- solutions/add.py
+++ solutions/add.py
@@ -1,3 +1,38 @@
-def add (a, b):
-    """ return the summation of two numbers  """
-    return (a+b)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for adding two numbers.
+
+Module contents: 
+    -add: adds two numbers and returns the result.
+    
+Created on 28.12.2024
+@author : Ridwan Ayinde
+
+"""
+
+
+
+
+def add (a:float, b:float) -> float:
+    """ Add two numbers.
+    
+    Parameters:
+        a:float, the first number
+        b:float, the second number
+        
+    Returns -> float: the sum of a and b
+    
+    Raises:
+        AssertionError: if any argument is not a number
+        
+    >>> add(4, 6)
+    10.0
+    >>> add(0, 0)
+    0.0
+    
+    """
+    # Validate inputs
+    assert isinstance (a, (int, float)), "a must be a number"
+    assert isinstance (b, (int, float)), "b must be a number"
+    return float(a+b)
--- solutions/tests/test_add.py
+++ solutions/tests/test_add.py
@@ -1,8 +1,46 @@
-from add import add
+#! usr/bin/python3
+# -*- coding: utf-8 -*-
 
-def test_add.py:
-    assert add(5,3) == 8
-    assert add (-1, 1) == 0
-    assert add (6, -5) == 1
+""" 
+Test for addition of two numbers
+
+Created on 28.12.2024
+@author : Ridwan Ayinde
+
+
+"""
+
+
+
+import unittest
+from solutions.add import add    #Import the function to test
+
+class TestAddFunction(unittest.TestCase):
     
-print ("All add tests passed!")
+    def test_add_positive_numbers(self):
+        """Test adding two positive numbers """
+        self.assertEqual(add(4,6), 10)
+    
+    def test_add_negative_numbers (self):
+        """Test add two negative numbers"""
+        self.assertEqual(add(-4,-5),-9)
+        
+    def test_add_mixed_sign_numbers(self):
+        """Test adding a positive and a negative number """
+        self.assertEqual (add(4,-5),-1)
+        
+    def test_add_zero (self):
+        """Test adding zero to a number"""
+        self.assertEqual(add(4,0), 4)
+        
+    def test_non_numbers(self):
+        """it should raise an AssertionError for non numbers input"""
+        with self.assertRaises(AssertionError):
+            add("a",5) # Invalid input: string instead of a number
+        
+    
+if __name__ == "__main__":
+    unittest.main()
+    
+
+    

ab52a7417d21f34eb10bbc42f7ad05bd3bf70989
Awaab98 <waleed72moh@gmail.com>
2024-12-28 15:38:32 +0100

    Check the py_tests ci-checks

---
+++ solutions/tests/test_matrix_2x2_inversion.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for testing the matrix_2x2_inversion function.
+
+Test categories:
+    - Standard test cases: test the function with typical inputs.
+    - Edge test cases: test the function with extreme inputs.
+    - Defensive tests: test the function with invalid inputs.
+
+
+Created on 2024-12-26
+Author: Awaab98
+"""

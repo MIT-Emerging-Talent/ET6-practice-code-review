40d5a8beedd2058db250e05a57337682ecda4d44
Ridwan Ayinde <ridwanbabatundeayinde@gmail.com>
2024-12-28 12:26:11 +0100

    Added add.py function and test_add.py

---
+++ solutions/add.py
@@ -0,0 +1,3 @@
+def add (a, b):
+    """ return the summation of two numbers  """
+    return (a+b)
---
+++ solutions/tests/test_add.py
@@ -0,0 +1,8 @@
+from add import add
+
+def test_add.py:
+    assert add(5,3) == 8
+    assert add (-1, 1) == 0
+    assert add (6, -5) == 1
+    
+print ("All add tests passed!")

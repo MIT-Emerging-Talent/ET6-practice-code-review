014798b72e42bad16bbcb166ea9b0cc3987237f6
doctorbanu <banuozyilmaz@gmail.com>
2024-12-29 16:51:32 -0500

    fix(prime-number-checker): correct comments for factor check logic

--- solutions/prime_number_checker.py
+++ solutions/prime_number_checker.py
@@ -21,8 +21,8 @@ def is_prime(n):
         bool: True if n is a prime number, False otherwise.
 
     Raises:
-        AssertionError: If n is not an integer.
-        AssertionError: If n is a negative number.
+        TypeError: If n is not an integer.
+        ValueError: If n is a negative number.
 
     Examples:
         >>> is_prime(1)
@@ -40,11 +40,11 @@ def is_prime(n):
         >>> is_prime(-6)
         Traceback (most recent call last):
             ...
-        AssertionError: The argument must be a non-negative integer.
+        ValueError: Input must be a non-negative integer.
         >>> is_prime(5.5)
         Traceback (most recent call last):
             ...
-        AssertionError: The argument should be an integer.
+        TypeError: Input must be an integer.
     """
     # Input validation
     assert isinstance(n, int), "The argument should be an integer."
@@ -55,7 +55,13 @@ def is_prime(n):
         print(f"{n} is NOT a prime number")
         return False
 
-    # Check for factors from 2 up to the square root of n
+    # Check for factors from 2 up to the square root of n :
+    # Because, if n has a factor larger than its square root, it must also have a smaller one.
+    # For example, if n = 36, then its factors are: 1 * 36, 2 * 18, 3 * 12, 4 * 9, and 6 * 6.
+    # Notice that the factors larger than 6 (its square root) have corresponding factors smaller
+    # than 6.
+    # Therefore, if no factors are found up to the square root, n must be prime.
+
     for i in range(2, int(n**0.5) + 1):
         # If n is divisible by any number in this range, it is not prime
         if n % i == 0:

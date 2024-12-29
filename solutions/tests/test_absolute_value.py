e163224244d8477d47528aa4f3aa72e0a334bbf8
Abdul Qader Dost <abdulqaderdost@yahoo.com>
2024-12-29 21:52:02 +0100

    Added a defensive test case (input as hello)

--- solutions/tests/test_absolute_value.py
+++ solutions/tests/test_absolute_value.py
@@ -14,20 +14,25 @@ from ..absolute_value import absolute_value
 class TestAbsoluteValue(unittest.TestCase):
     """Test the absolute_value function"""
 
-    def test_0(self):
+    def test_absolute_value_of_zero(self):
         """It should return 0 when the input is 0"""
         actual = absolute_value(0)
         expected = 0
         self.assertEqual(actual, expected)
 
-    def test_1(self):
-        """It should return 10 when the input is 10"""
+    def test_absolute_value_of_positive_number(self):
+        """It should return the positive value of a number"""
         actual = absolute_value(10)
         expected = 10
         self.assertEqual(actual, expected)
 
-    def test_2(self):
-        """It should return 10 when the input is -10"""
+    def test_absolute_value_of_negative_number(self):
+        """It should return the positive value of a negative number"""
         actual = absolute_value(-10)
         expected = 10
         self.assertEqual(actual, expected)
+
+    def test_type_error_for_non_number_input(self):
+        """It should raise a TypeError for non-number input"""
+        with self.assertRaises(TypeError):
+            absolute_value("hello")

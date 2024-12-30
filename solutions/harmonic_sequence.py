ebe0c804176c26ea86f52870bc0142d5d09cdfba
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-30 20:14:54 +0400

    Add more comments

--- solutions/harmonic_sequence.py
+++ solutions/harmonic_sequence.py
@@ -11,7 +11,9 @@ def harmonic_sequence(a, d, n):
     List: List containing the harmonic sequence
 
     Assertions:
-    - a, d, n must be integer
+    - a must be integer
+    - d must be integer
+    - n must be integer
 
     Examples:
     >>> harmonic_sequence(1, 1, 1)
@@ -30,14 +32,20 @@ def harmonic_sequence(a, d, n):
     [1.0, 0.2, 0.05, 0.02, 0.01, 0.004, 0.0016, 0.00064, 0.000256, 0.0001024]
 
     """
+    # Assert an error if any input is not integer
     assert isinstance(a, int), "A must be integer"
     assert isinstance(d, int), "D must be integer"
     assert isinstance(n, int), "N must be integer"
+
+    # Create an empty list to save the sequence
     sequence = []
+    # If n is less than or equal to 0, return empty list
     if n <= 0:
         return sequence
+    # If a and d are equal to 0, return a list of n zeros
     if a == 0 and d == 0:
         return [0] * n
+    # If all inputs are greater than 1, return the sequence
     for i in range(1, n + 1):
         term = 1 / (a + (i - 1) * d)
         sequence.append(term)

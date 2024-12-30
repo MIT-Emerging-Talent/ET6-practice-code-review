095a353a68cb21c84836d652d07d53065dd28103
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-30 19:44:25 +0400

    Harmonic Sequence code with test

--- solutions/harmonic_sequence.py
+++ solutions/harmonic_sequence.py
@@ -10,4 +10,29 @@ def harmonic_sequence(a, d, n):
     Returns:
     List: List containing the harmonic sequence
 
+    Examples:
+    >>> harmonic_sequence(1, 1, 1)
+    [1.0]
+    >>> harmonic_sequence(1, 1, 0)
+    []
+    >>> harmonic_sequence(0, 0, 5)
+    [0.0, 0.0, 0.0, 0.0, 0.0]
+    >>> harmonic_sequence(1, 1, -1)
+    []
+    >>> harmonic_sequence(1, 1, 5)
+    [1.0, 0.5, 0.3333333333333333, 0.25, 0.2]
+    >>> harmonic_sequence(2, 2, 3)
+    [0.5, 0.3333333333333333, 0.25]
+    >>> harmonic_sequence(1, 5, 10)
+    [1.0, 0.2, 0.05, 0.02, 0.01, 0.004, 0.0016, 0.00064, 0.000256, 0.0001024]
+
     """
+    sequence = []
+    if n <= 0:
+        return sequence
+    if a == 0 and d == 0:
+        return [0] * n
+    for i in range(1, n + 1):
+        term = 1 / (a + (i - 1) * d)
+        sequence.append(term)
+    return sequence
--- solutions/tests/test_harmonic_sequence.py
+++ solutions/tests/test_harmonic_sequence.py
@@ -1,4 +1,31 @@
 import unittest
+
 from ..harmonic_sequence import harmonic_sequence
 
+
 class TestAsciiCode(unittest.TestCase):
+    def test_min_input1(self):
+        """Test case when all inputs equal to 1"""
+        self.assertEqual(harmonic_sequence(1, 1, 1), [1.0])
+
+    def test_n_equal_0(self):
+        """Test case when n is equal to 0"""
+        self.assertEqual(harmonic_sequence(1, 1, 0), [])
+
+    def test_a_and_d_equal_0(self):
+        """Test case when a and d are equal to 0"""
+        self.assertEqual(harmonic_sequence(0, 0, 5), [0, 0, 0, 0, 0])
+
+    def test_n_negative(self):
+        """Test case when n is negative"""
+        self.assertEqual(harmonic_sequence(1, 1, -1), [])
+
+    def test_a_and_d_equal_1(self):
+        """Test a and d are equal 1"""
+        self.assertEqual(
+            harmonic_sequence(1, 1, 5), [1.0, 0.5, 0.3333333333333333, 0.25, 0.2]
+        )
+
+    def test_all_inputs_greater_than_1(self):
+        """Test when all inputs are integer and greater than 1"""
+        self.assertEqual(harmonic_sequence(2, 2, 3), [0.5, 0.25, 0.16666666666666666])

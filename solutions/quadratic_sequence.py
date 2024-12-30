509a789ee8375af80277bae955bb2ebb18be5a4d
Omnia-Agabani <omniaagabani99@gmail.com>
2024-12-30 18:50:19 +0400

    Quadratic Sequence File

---
+++ solutions/quadratic_sequence.py
@@ -0,0 +1,14 @@
+def quadratic_sequence(a, b, c, n):
+    """
+    The function creates quadratic sequence, based on formula: A(n): an^2 + bn + c
+
+    Parameters:
+    a (int): Coefficient of n^2
+    b (int): Coefficient of n
+    c (int): Constant term
+    n (int): Number of terms to generate
+
+    Returns:
+    List: List containing the quadratic sequence
+
+    """
---
+++ solutions/tests/test_quadratic_sequence.py
@@ -0,0 +1,4 @@
+import unittest
+from ..quadratic_sequence import quadratic_sequence
+
+class TestAsciiCode(unittest.TestCase):

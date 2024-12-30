7a2557715bc726ad65f836bf3f1ad39baff188c7
Noorelsalam <noorelsalamabdo@gmail.com>
2024-12-30 14:14:17 +0100

    Created docstring and ran the first failing test

---
+++ solutions/tests/test_convert_string_to_uppercase.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for testing the convert_string_to_uppercase function
+
+Created on 29 Dec 2024
+@author: Noorelsalam Almakki
+"""
+
+import unittest
+
+from ..convert_string_to_uppercase import convert_string_to_uppercase
+
+
+class TestConvertStringToUppercase(unittest.TestCase):
+    """A class for testing the convert_string_to_uppercase function"""
+
+    def test_empty_string(self):
+        """Test the convert_string_to_uppercase function with an empty string"""
+        self.assertEqual(convert_string_to_uppercase(""), "")

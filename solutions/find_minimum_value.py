78bee4d59d863b8c111076b7456278ef5b0616d3
allukmon1 <allukmon1@gmail.com>
2024-12-30 07:29:24 +0100

    resolve-assertion-for-values-is-int-or-float

--- solutions/find_minimum_value.py
+++ solutions/find_minimum_value.py
@@ -4,19 +4,20 @@ created 2024-12-28
 @author: Lukmon Alao
 """
 
+
 def find_minimum_value(values: list):
     """Returns the lowest value from a list of numbers input
-    
+
     takes a list of numbers and return a single number which is the lowest value from the list.
-    
+
     Parameters:
         values: list, the input values to process
-        
+
     Returns -> number: the lowest value from the list of numbers.
-    
+
     Raises:
-        AssertionError: if input is not a list and numerical
-        
+        AssertionError: if input is not a list and non-numeric data
+
     Examples:
         >>> find_minimum_value([1, 2, 3, 4])
         1
@@ -25,7 +26,11 @@ def find_minimum_value(values: list):
         >>> find_minimum_value([1.2, 3.4, 2.7, 0.5])
         0.5
     """
-    assert isinstance(values, list)  #verifies that 'values' is a list 
-    ordered_list_of_values = sorted(values) #returns the list in ascending order
-    min = ordered_list_of_values[0]  #gives the minimum value from the ordered list
+
+    assert isinstance(values, list)  # verifies that 'values' is a list
+    assert all(
+        isinstance(item, (int, float)) for item in values
+    )  # "values must be an int or a float"
+    ordered_list_of_values = sorted(values)  # returns the list in ascending order
+    min = ordered_list_of_values[0]  # gives the minimum value from the ordered list
     return min

6db4edc644ac50c03c85e1b41c0b9e0447ba5f3c
Ridwan Ayinde <ridwanbabatundeayinde@gmail.com>
2024-12-31 14:16:15 +0100

    Add add function implementation and itest cases

--- solutions/add.py
+++ solutions/add.py
@@ -12,27 +12,25 @@ Created on 28.12.2024
 """
 
 
+def add(a: float, b: float) -> float:
+    """Add two numbers.
 
-
-def add (a:float, b:float) -> float:
-    """ Add two numbers.
-    
     Parameters:
         a:float, the first number
         b:float, the second number
-        
+
     Returns -> float: the sum of a and b
-    
+
     Raises:
         AssertionError: if any argument is not a number
-        
+
     >>> add(4, 6)
     10.0
     >>> add(0, 0)
     0.0
-    
+
     """
     # Validate inputs
-    assert isinstance (a, (int, float)), "a must be a number"
-    assert isinstance (b, (int, float)), "b must be a number"
-    return float(a+b)
+    assert isinstance(a, (int, float)), "a must be a number"
+    assert isinstance(b, (int, float)), "b must be a number"
+    return float(a + b)

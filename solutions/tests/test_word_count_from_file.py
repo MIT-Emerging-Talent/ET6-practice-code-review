b639bd10ab00f16b714c503f55490f38fb9f33f8
sajaabusafia <sajakhalee595@gmail.com>
2024-12-31 20:01:15 +0200

    edits

--- solutions/tests/test_word_count_from_file.py
+++ solutions/tests/test_word_count_from_file.py
@@ -13,7 +13,6 @@ Returns -> The total number of words in the file.
 Raises: AssertionError
 """
 
-
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.append(project_root)
 

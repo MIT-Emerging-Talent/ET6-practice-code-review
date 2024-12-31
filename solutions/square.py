6d7e4cf8ed0b154398fd7306c86968a43b372978
Ayodele Ayomide <66219396+Bigbadwolf21@users.noreply.github.com>
2024-12-31 04:02:30 +0100

    Created square.py

---
+++ solutions/square.py
@@ -0,0 +1,12 @@
+# The square root function of our calculator application
+def square(num):
+    """
+    Calculate the square of a number.
+
+    Parameters:
+    number (float): The number to be squared.
+
+    Returns:
+    float: The square of the input number.
+    """
+    return num ** 2

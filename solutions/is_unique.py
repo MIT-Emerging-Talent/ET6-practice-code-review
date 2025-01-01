37f882b93f93f12a0d015dd46d206e5ebf5bbef6
ava <c.aavah@gmail.com>
2025-01-01 19:25:58 +0300

    Add module header

--- solutions/is_unique.py
+++ solutions/is_unique.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for checking if all characters in a string are unique.
+
+Module contents:
+    - is_unique: Verifies if all characters in a given string are unique.
+
+Created on 2025-01-01
+Author: Ava Abdullah
+"""
+
+
 def is_unique(s: str) -> bool:
     if not isinstance(s, str):
         raise ValueError("Input must be a string.")
     return len(set(s)) == len(s)
-
-
-print(is_unique("3ss!20"))

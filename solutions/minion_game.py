2cc101323fab20ffc4dd03096199279eb327ca04
salemAmassi <salem.amassi@gmail.com>
2025-01-01 08:24:07 +0200

    Modify challenge

--- solutions/minion_game.py
+++ solutions/minion_game.py
@@ -1,19 +1,5 @@
 def minion_game(string):
     # your code goes here
-    string = string.lower()
-    kevin_score = 0
-    stuart_score = 0
-    vowels = ['a', 'u','o','i','e']
-    kevin_words = {}
-    stuart_words = {}
-    for letter in string:
-        if letter in vowels:
-            kevin_words[letter] += 1
-            kevin_score += kevin_words[letter]
-        else:
-            stuart_words[letter] += 1 
-            stuart_score += stuart_words[letter]
-
-    return max(kevin_score, stuart_score)
+    return ""
 
 minion_game('BANANA')

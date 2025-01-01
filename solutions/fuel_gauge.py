01ceeee11d5b1bd21f753fe257877295ddd8cf24
Goutbi <agoutbi10@gmail.com>
2025-01-01 06:11:28 +0400

    unittest added

--- solutions/fuel_gauge.py
+++ solutions/fuel_gauge.py
@@ -1,13 +1,13 @@
 def fuel_gauge(x, y):
-    """
-   
-    """
+    """ """
     if y == 0 or x > y:
-        raise ValueError("Invalid input: y must be greater than 0 (tank size can't be zero) and x must be less than or equal to y(fuel can't be more than the tank capacity).")
-    
+        raise ValueError(
+            "Invalid input: y must be greater than 0 (tank size can't be zero) and x must be less than or equal to y(fuel can't be more than the tank capacity)."
+        )
+
     percentage = (x / y) * 100
     if percentage >= 99:
-        return 'F'
+        return "F"
     elif percentage <= 1:
-        return 'E'
+        return "E"
     return int(percentage)
--- solutions/tests/test_fuel_gauge.py
+++ solutions/tests/test_fuel_gauge.py
@@ -1,3 +1,37 @@
 import unittest
+
 from ..fuel_gauge import fuel_gauge
 
+
+class TestFuelGauge(unittest.TestCase):
+    """Test cases for the fuel_gauge function."""
+
+    def test_full(self):
+        """Test cases where the percentage is 99% or more."""
+        self.assertEqual(fuel_gauge(99, 100), "F")
+        self.assertEqual(fuel_gauge(100, 100), "F")
+
+    def test_empty(self):
+        """Test cases where the percentage is 1% or less."""
+        self.assertEqual(fuel_gauge(1, 100), "E")
+        self.assertEqual(
+            fuel_gauge(
+                0,
+                100,
+            ),
+            "E",
+        )
+
+    def test_percentage(self):
+        """Test cases where the result is a percentage."""
+        self.assertEqual(fuel_gauge(50, 100), 50)
+        self.assertEqual(fuel_gauge(25, 50), 50)
+
+    def test_invalid_input(self):
+        """Test cases for invalid inputs."""
+        with self.assertRaises(ValueError):
+            fuel_gauge(3, 0)
+        with self.assertRaises(ValueError):
+            fuel_gauge(4, 3)
+        with self.assertRaises(ValueError):
+            fuel_gauge("abc", "def")

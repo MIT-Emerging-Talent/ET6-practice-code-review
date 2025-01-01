88ec7183d56a895554295facceed06ab3eba1540
Goutbi <agoutbi10@gmail.com>
2025-01-01 07:50:22 +0400

    added unittests and comments

--- solutions/fuel_gauge.py
+++ solutions/fuel_gauge.py
@@ -1,13 +1,20 @@
 def fuel_gauge(x, y):
-    """ """
+    """
+    This is a function that calculates the percentage of fuel in a tank.
+
+    Parameters:
+    X= fuel in the tank (x value must be less than or equal to y)
+    Y= tank capacity (y value must be greater than 0)"""
     if y == 0 or x > y:
-        raise ValueError(
-            "Invalid input: y must be greater than 0 (tank size can't be zero) and x must be less than or equal to y(fuel can't be more than the tank capacity)."
-        )
+        """This is a condition that checks if the tank size is zero or if the fuel is more than the tank capacity."""
+        return "Invalid input"
 
     percentage = (x / y) * 100
-    if percentage >= 99:
+    """This is a formula that calculates the percentage of fuel in the tank."""
+    if percentage >= 90:
         return "F"
-    elif percentage <= 1:
+    elif percentage > 10:
+        return "M"
+    elif percentage <= 10:
         return "E"
     return int(percentage)
--- solutions/tests/test_fuel_gauge.py
+++ solutions/tests/test_fuel_gauge.py
@@ -7,31 +7,24 @@ class TestFuelGauge(unittest.TestCase):
     """Test cases for the fuel_gauge function."""
 
     def test_full(self):
-        """Test cases where the percentage is 99% or more."""
-        self.assertEqual(fuel_gauge(99, 100), "F")
+        """Test cases where the percentage is 90% or more it should result in F."""
+        self.assertEqual(fuel_gauge(90, 100), "F")
         self.assertEqual(fuel_gauge(100, 100), "F")
+        self.assertEqual(fuel_gauge(97, 100), "F")
 
     def test_empty(self):
-        """Test cases where the percentage is 1% or less."""
-        self.assertEqual(fuel_gauge(1, 100), "E")
-        self.assertEqual(
-            fuel_gauge(
-                0,
-                100,
-            ),
-            "E",
-        )
+        """Test cases where the percentage is 10% or less it should result E."""
+        self.assertEqual(fuel_gauge(10, 100), "E")
+        self.assertEqual(fuel_gauge(0, 100), "E")
+        self.assertEqual(fuel_gauge(2, 100), "E")
 
     def test_percentage(self):
-        """Test cases where the result is a percentage."""
-        self.assertEqual(fuel_gauge(50, 100), 50)
-        self.assertEqual(fuel_gauge(25, 50), 50)
+        """Test cases where the result is between 90 and 10 it should be M."""
+        self.assertEqual(fuel_gauge(71, 100), "M")
+        self.assertEqual(fuel_gauge(23, 100), "M")
+        self.assertEqual(fuel_gauge(10, 50), "M")
 
     def test_invalid_input(self):
         """Test cases for invalid inputs."""
-        with self.assertRaises(ValueError):
-            fuel_gauge(3, 0)
-        with self.assertRaises(ValueError):
-            fuel_gauge(4, 3)
-        with self.assertRaises(ValueError):
-            fuel_gauge("abc", "def")
+        self.assertEqual(fuel_gauge(6, 0), "Invalid input")
+        self.assertEqual(fuel_gauge(7, -1), "Invalid input")

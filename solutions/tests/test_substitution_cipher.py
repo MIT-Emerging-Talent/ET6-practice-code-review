a7aed16869ab47a9e44ab4cf1055d4618497b3c6
ayhm01 <ayhamalser2005@gmail.com>
2025-01-01 21:09:31 +0200

    Added unit tests for substitution cipher

---
+++ solutions/tests/test_substitution_cipher.py
@@ -0,0 +1,37 @@
+import unittest
+
+from solutions.substitution_cipher import cipher
+
+
+class TestCipher(unittest.TestCase):
+    """
+    testing cipher function
+    """
+
+    def test_lowercase(self):
+        """
+        testing lower case encryption
+        """
+        self.assertEqual(cipher("abc"), "def")
+
+    def test_mixed_cases(self):
+        """
+        testing lower and upper case mixed letters
+        """
+        self.assertEqual(cipher("AbCd"), "DeFg")
+
+    def test_edge_cases(self):
+        """
+        testing edge cases such as reaching the end of the alphabets
+        """
+        self.assertEqual(cipher("xyz"), "abc")
+
+    def test_invalid_chars(self):
+        """
+        testing that ivalid chars remain unchanged when mixed with alphabets
+        """
+        self.assertEqual(cipher("Hello@123!"), "Khoor@123!")
+
+
+if __name__ == "__main__":
+    unittest.main()

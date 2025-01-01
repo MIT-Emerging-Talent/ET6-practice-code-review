5d5e16fca217411d6528de5a796461a06912989c
Mayar-Agab <mayar_agab@outlook.com>
2025-01-01 19:43:38 +0200

    Extre changed and CI checks

--- solutions/tests/test_arithmetic_sequence.py
+++ solutions/tests/test_arithmetic_sequence.py
@@ -4,18 +4,19 @@
 Created on Monday/30/December/2024
 @author:Mayar Ali
 """
+
 import unittest
 
-#---imports & test class before documenting and testing---
+# ---imports & test class before documenting and testing---
 # from ..arithmetic_sequence import arithmetic_sequence
 # class TestArithmeticSequence(unittest.TestCase):
-#---imports & test class after documenting and testing---
+# ---imports & test class after documenting and testing---
 
-from..arithmetic_sequence import arithmetic_sequence
+from ..arithmetic_sequence import arithmetic_sequence
+
 
 class TestArithmeticSequence(unittest.TestCase):
     """Test the arithmetic_sequence function"""
- 
     def test_0(self):
         "It should evaluate 0 to []"
         self.assertEqual(arithmetic_sequence(0), [])
@@ -26,13 +27,13 @@ class TestArithmeticSequence(unittest.TestCase):
 
     def test_2(self):
         "Should evaluate 2 to [0,1]"
-        self.assertEqual(arithmetic_sequence(2) ,[0,1])
+        self.assertEqual(arithmetic_sequence(2), [0, 1])
 
     def test_3(self):
         "Should evaluate 3 to [0, 1, 5]"
-        self.assertEqual(arithmetic_sequence(3) ,[0, 1, 4])
+        self.assertEqual(arithmetic_sequence(3), [0, 1, 4])
 
-#defensive cases
+    # defensive cases
 
     def test_0_less_than_0(self):
         """It should raise an assertion error if the argument is less than 0"""
@@ -40,26 +41,21 @@ class TestArithmeticSequence(unittest.TestCase):
             arithmetic_sequence(-1)
 
     def test_1_not_an_integer(self):
-        """It should raise an assertion error if the argument is not an integer"""
+        """Should raise an assertion error if the argument is not an integer"""
         with self.assertRaises(AssertionError):
-            arithmetic_sequence(1.0)    
+            arithmetic_sequence(1.0)
 
     def test_2_not_an_integer(self):
-        """It should raise an assertion error if the argument is not an integer"""
+        """Should raise an assertion error if the argument is not an integer"""
         with self.assertRaises(AssertionError):
-            arithmetic_sequence([1 ,2 ,3])    
+            arithmetic_sequence(["1", 2, 3])
 
-    def test_3_not_an_integer(self):
-        """It should raise an assertion error if the argument is not an integer"""
-        with self.assertRaises(AssertionError):
-            arithmetic_sequence(["1" ,2 ,3]) 
-
-#Edge case
+    # Edge case
 
     def test_a_big_number(self):
         "Should evaluate 100 to [0, 1, 5]"
         self.assertEqual(
-            arithmetic_sequence(100) ,
+            arithmetic_sequence(100),
             [
                 0,
                 1,
@@ -161,7 +157,5 @@ class TestArithmeticSequence(unittest.TestCase):
                 289,
                 292,
                 295,
-            ]
-    )
-
-
+            ],
+        )

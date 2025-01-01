392b1bcd5fbf61aa0a0c8abda6a9ba5628122c5f
Karim Kamara <kamarak388@gmail.com>
2025-01-01 14:00:45 +0000

    Adding test file

---
+++ solutions/tests/test_find_largest_number.py
@@ -0,0 +1,8 @@
+from find_largest_number import find_largest_number
+
+# Test cases
+print(find_largest_number([3, 1, 7, 0, 5]))  # Output: 7
+print(find_largest_number([-10, -20, -3, -50]))  # Output: -3
+print(find_largest_number([0, 0, 0, 0]))  # Output: 0
+print(find_largest_number([100, 200, 300]))  # Output: 300
+print(find_largest_number([5]))  # Output: 5

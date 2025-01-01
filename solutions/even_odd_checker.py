cf708ce5399cb7340a11fbc47ac00a970a822e8b
robiel0143 <robmilthon24@gmail.com>
2025-01-01 13:56:52 +0300

    Even odd Checker Challenge

--- solutions/even_odd_checker.py
+++ solutions/even_odd_checker.py
@@ -12,19 +12,19 @@ Author: Robel Mengsteab
 
 def even_odd_checker(n: int) -> str:
     """Checks if a number is even or odd.
-    
+
     Determines if the input number is even or odd and validates the input.
-    
+
     Parameters:
         n: int, the number to check.
-        
+
     Returns -> str: "Even" if the number is even, "Odd" if the number is odd.
-    
+
     Raises:
         AssertionError: if input is not an integer or a whole number float.
         ValueError: if no input is provided
         TypeError: if input is a string or invalid type.
-    
+
     Examples:
         >>> even_odd_checker(2)
         'Even'
@@ -39,24 +39,23 @@ def even_odd_checker(n: int) -> str:
         >>> even_odd_checker(-1)
         'Odd'
     """
-    
     # Checks if there is no value given
     if n is None:
         raise ValueError("Input cannot be None")
     # Raises a TypeError if the input is a String
     if isinstance(n, str):
         raise TypeError("Input cannot be a string")
-    
+
     # Checks if the given input is an integer or a whole float
     assert isinstance(n, (int, float)), "Input must be an integer or a whole float"
-    
+
     # Check if float is not a whole number
     if n != int(n):
         raise AssertionError("Input must be an integer or a whole float")
-    
+
     # Convert to integer if it's a whole number float
     n = int(n)
-    
+
     # Check if the number is even or odd
     if n % 2 == 0:
         return "Even"

60aafa60c5f2d32f8b82579c2e3dbd9cc1022c2e
Goutbi <agoutbi10@gmail.com>
2025-01-02 09:16:28 +0400

    resolving python formatting -1

--- solutions/fuel_gauge.py
+++ solutions/fuel_gauge.py
@@ -4,7 +4,7 @@ def fuel_gauge(x: int, y: int) -> str:
 
     Parameters:
         x (int): Fuel in the tank (must be ≤ y).
-        y (int): Tank capacity (must be > 0, the tank can't be 0 or a negative number).
+        y (int): Tank capacity (must be > 0).
 
     Returns:
         str: 

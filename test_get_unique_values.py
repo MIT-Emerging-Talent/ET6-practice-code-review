d36ab70ecd1d5c26c1ad9216c73a7e36cb863379
Derek Karungani <derekkarungani@gmail.com>
2025-01-02 01:07:02 -0600

    Add get_unique_values function and tests. Fixes #25

---
+++ test_get_unique_values.py
@@ -0,0 +1,17 @@
+import unittest
+from get_unique_values import get_unique_values
+
+class TestGetUniqueValues(unittest.TestCase):
+    def test_unique_values(self):
+        self.assertEqual(get_unique_values([1, 2, 2, 3]), {1, 2, 3})
+
+    def test_empty_list(self):
+        self.assertEqual(get_unique_values([]), set())
+
+    def test_invalid_input(self):
+        with self.assertRaises(ValueError):
+            get_unique_values("not a list")
+
+if __name__ == "__main__":
+    unittest.main()
+

a3fc74beddbec6eed04504265b85985f96fdf383
HUDA ALAMASSI <bussiness.hn24@outlook.com>
2025-01-02 06:22:24 +0200

    checking the code strategy

--- solutions/is_palindrome.py
+++ solutions/is_palindrome.py
@@ -12,7 +12,6 @@ Created on 2024-12-30
 @author: Huda Alamassi
 """
 
-# To identify and exclude punctuation characters by checking their Unicode category
 import unicodedata
 
 
@@ -26,16 +25,12 @@ def is_palindrome(text_to_check: str) -> bool:
 
     Arguments:
     text_to_check (str): The input string to be checked.
-    - Must be a valid string (an assertion will raise an error for non-strings).
-
 
     Returns:
     bool:
-        - True if the input string is a palindrome (reads the same backward
-        as forward after ignoring spaces, punctuation, and case differences).
-        - False otherwise.
-        - An empty string or a string with only spaces/punctuation is not
-        considered a palindrome.
+        - True if the string is a palindrome after removing spaces, punctuation,
+        and case differences.
+        - False if the string is not a palindrome or contains only spaces/punctuation.
 
     Raises:
     AssertionError: If the input is not a string.
@@ -54,33 +49,25 @@ def is_palindrome(text_to_check: str) -> bool:
         True
 
     """
-    # Assert that the input is a string
+    # validate input type
     assert isinstance(text_to_check, str), "text_to_check must be a string"
 
-    # Convert to lowercase to ensure case-insensitivity
+    # ensure case-insensitivity
     text_to_check = text_to_check.lower()
 
-    # Remove all types of whitespace (spaces, tabs, line breaks) from the string
-    # This ensures that whitespaces are ignored when checking for palindromes
+    # whitespace should not impact the palindrome check
     text_to_check = "".join(text_to_check.split())
 
-    # To ensure an empty string or a string with space/spaces considered non-palindrome
-    if text_to_check == "":
-        return False
-
-    # Remove punctuation
+    # punctuation should not impact the palindrome check
     cleaned_text = ""
     for text_char in text_to_check:
         if not unicodedata.category(text_char).startswith("P"):
             cleaned_text += text_char
 
-    # To ensure a string with only punctuation is considered non-palindrome
+    # exclude empty or non-informative strings
     if cleaned_text == "":
         return False
 
-    # Reverse_string
     reversed_text = cleaned_text[::-1]
 
-    return (
-        reversed_text == cleaned_text
-    )  # Check if the string is the same forwards and backwards
+    return reversed_text == cleaned_text

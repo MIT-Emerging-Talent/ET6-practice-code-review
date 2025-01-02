e30741ee35b3172b4d44bb25cb75c52bd4aeee17
Viktoriya Haiduk <vitadrug@gmail.com>
2025-01-02 15:51:58 +0200

    Adding doctsring to each unitest

--- solutions/tests/test_sort_numbers.py
+++ solutions/tests/test_sort_numbers.py
@@ -17,15 +17,27 @@ from solutions.sort_numbers import sort_numbers
 
 class TestSortNumbers(unittest.TestCase):
     def test_sorted_numbers(self):
+        """
+        Test if the func. correctly sorts a list of integers in asc order.
+        """
         self.assertEqual(sort_numbers([4, 1, 3, 2]), [1, 2, 3, 4])
 
     def test_floats(self):
+        """
+        Test if the func. correctly sorts a list of floats in asc order.
+        """
         self.assertEqual(sort_numbers([4.5, 2.3, 1.7, 3.9]), [1.7, 2.3, 3.9, 4.5])
 
     def test_mixed_numbers(self):
+        """
+        Test if the func. sorts a list cont. both integers and floats.
+        """
         self.assertEqual(sort_numbers([4, 2.5, 3, 1.2]), [1.2, 2.5, 3, 4])
 
     def test_invalid_input(self):
+        """
+        Test if the func. raises a ValueError for a list with non-numeric.
+        """
         with self.assertRaises(ValueError) as context:
             sort_numbers(["a", None, 3])
         self.assertEqual(
@@ -33,13 +45,19 @@ class TestSortNumbers(unittest.TestCase):
         )
 
     def test_empty_list(self):
+        """
+        Test if the func. raises a ValueError for an empty list.
+        """
         with self.assertRaises(ValueError) as context:
             sort_numbers([])
         self.assertEqual(
-            str(context.exception), "The list is empty. Please provide numbers to sort."
+            str(context.exception), "The list is empty. Provide numbers."
         )
 
     def test_negative_numbers(self):
+        """
+        Test if the func. correctly sorts a list of neg. numbers.
+        """
         self.assertEqual(sort_numbers([-3, -1, -4, -2]), [-4, -3, -2, -1])
 
 

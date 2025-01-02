f27bfb783b02ec64fda22da821d0eb1d11ed3e79
RAFAA ALI <rafaa202@outlook.com>
2025-01-02 23:13:00 +0400

    tests have been added

--- solutions/FooBar_code.py
+++ solutions/FooBar_code.py
@@ -1,4 +1,55 @@
+"""
+Creates a sequence where numbers divisible by 4 return 'Foo',
+numbers divisible by 6 return 'Bar', numbers divisible by both
+return 'FooBar', and other numbers remain unchanged.
+
+Author: Rafaa Ali
+Created date: 1.1.2025
+"""
+
+
 def FooBar_code(n: int):
+    """This function, `FooBar_code`, creates a sequence that replaces numbers
+    based on their divisibility by 4 and 6.
+
+    Parameters:
+      n (int):the last number in the list
+
+    Returns:
+      List containing the sequence with 'Foo', 'Bar', 'FooBar' or numbers
+
+    assertion:
+    n must be integer otherwise error
+
+
+    example:
+    Edge cases:
+    >>> FooBar_code(1)
+    [1]
+    >>> FooBar_code(0)
+    []
+    >>> FooBar_code(-1)
+    []
+
+    Standard cases:
+    >>> FooBar_code(8)
+    [1, 2, 3, 'Foo', 5, 'Bar', 7, 'Foo']
+    >>> FooBar_code(12)
+    [1, 2, 3, 'Foo', 5, 'Bar', 7, 'Foo', 9, 10, 11, 'FooBar']
+    >>> FooBar_code(16)
+    [1, 2, 3, 'Foo', 5, 'Bar', 7, 'Foo', 9, 10, 11, 'FooBar',
+    13, 14, 15, 'Foo']
+
+    Defensive cases:
+    >>> FooBar_code("rafaa")
+    AssertionError: N must be integer
+    >>> FooBar_code(5.5)
+    AssertionError: N must be integer
+    >>> FooBar_code([1, 2, 3])
+    AssertionError: N must be integer
+
+
+    """
     assert isinstance(n, int), "N must be integer"
     result = []
     for i in range(1, n + 1):
--- solutions/tests/test_FooBar_code.py
+++ solutions/tests/test_FooBar_code.py
@@ -4,7 +4,60 @@ from ..FooBar_code import FooBar_code
 
 
 class TestFooBar(unittest.TestCase):
-    def test_int(self):
+    # Edge cases:
+    def test_int1(self):
+        self.assertEqual(FooBar_code(1), [1])
+
+    def test_int0(self):
+        self.assertEqual(FooBar_code(0), [])
+
+    def test_negative_int(self):
+        self.assertEqual(FooBar_code(-1), [])
+
+    # Standard cases:
+    def test_int8(self):
+        self.assertEqual(FooBar_code(8), [1, 2, 3, "foo", 5, "Bar", 7, "foo"])
+
+    def test_int12(self):
         self.assertEqual(
             FooBar_code(12), [1, 2, 3, "foo", 5, "Bar", 7, "foo", 9, 10, 11, "FooBar"]
         )
+
+    def test_is_int(self):
+        self.assertEqual(
+            FooBar_code(16),
+            [
+                1,
+                2,
+                3,
+                "foo",
+                5,
+                "Bar",
+                7,
+                "foo",
+                9,
+                10,
+                11,
+                "FooBar",
+                13,
+                14,
+                15,
+                "foo",
+            ],
+        )
+
+    # defensive cases
+    def test_string_input(self):
+        """It should raise AssertionError for string input"""
+        with self.assertRaises(AssertionError):
+            FooBar_code("rafaa")
+
+    def test_is_float_input(self):
+        """It should raise AssertionError for float input"""
+        with self.assertRaises(AssertionError):
+            FooBar_code(5.5)
+
+    def test_is_list_input(self):
+        """It should raise AssertionError for list input"""
+        with self.assertRaises(AssertionError):
+            FooBar_code([1, 2, 3])

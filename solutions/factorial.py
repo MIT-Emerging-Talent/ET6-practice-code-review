637180284de2d9fb7f3118272dc2483f5f29e983
Abdul Qader Dost <abdulqaderdost@yahoo.com>
2025-01-03 15:01:57 +0100

    Changed the line length of function docstring description.

--- solutions/factorial.py
+++ solutions/factorial.py
@@ -13,9 +13,10 @@ Created on 01-03-2024
 
 
 def factorial(number: int) -> int:
-    """The factorial function computes the factorial of a given non-negative integer.
-    The factorial of a number (n!) is the result of multiplying all whole numbers
-    from 1 up to number. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+    """The factorial function computes the factorial of a given
+    non-negative integer. The factorial of a number (n!) is the
+    result of multiplying all whole numbers from 1 up to number.
+    For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
 
     Parameters:
       number: int, A non-negative integer whose factorial is to be computed.

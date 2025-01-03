759f41c97bc862d93fb99665249a753a9b67d48c
jola-ds <jolajayy@gmail.com>
2025-01-03 16:02:36 +0100

    added function docstring and doctests

--- solutions/length_longest_substring.py
+++ solutions/length_longest_substring.py
@@ -13,3 +13,23 @@ Created on: 2025-01-03
 Author: Jola-Moses
 """
 def length_longest_substring(s: str) -> int:
+    """
+    Returns the length of the longest substring without repeating characters.
+
+    Parameters:
+        s: str, the input string for which to find the longest substring
+
+    Returns -> int, the length of the longest substring without repeating characters
+
+    Raises:
+        AssertionError: if s is not a string
+
+    >>> length_longest_substring("abcabcbb")
+    3
+    
+    >>> length_longest_substring("abcdef")
+    6
+    
+    >>> length_longest_substring("aaaaaa")
+    1
+    """

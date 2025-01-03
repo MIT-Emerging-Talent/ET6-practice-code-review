16a27268dcf91f2ba1d2397f8c657f48242a5c7c
Abdul Qader Dost <abdulqaderdost@yahoo.com>
2025-01-03 12:11:52 +0100

    Added docstring for the factorial function

--- solutions/factorial.py
+++ solutions/factorial.py
@@ -10,3 +10,32 @@ Created on 01-03-2024
 
 @author: Abdul Qader Dost
 """
+
+
+def factorial(number: int) -> int:
+    """The factorial function computes the factorial of a given non-negative integer.
+    The factorial of a number (n!) is the result of multiplying all whole numbers
+    from 1 up to number. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+
+    Parameters:
+      number: int, A non-negative integer whose factorial is to be computed.
+
+    Returns -> int, Returns the factorial of the input number.
+
+    Raises:
+      ValueError: If the input is a negative integer.
+      TypeError: If the input is not an integer.
+
+
+    >>> factorial(1)
+    1
+
+    >>> factorial(2)
+    2
+
+    >>> factorial(3)
+    6
+
+    >>> factorial(4)
+    24
+    """

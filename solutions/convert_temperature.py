2d6f1b9e7207c756c6ce76d3043d6c26c7b0f303
bigfudge22 <eng.amroshkh@gmail.com>
2025-01-03 12:43:32 +0200

    function2

--- solutions/convert_temperature.py
+++ solutions/convert_temperature.py
@@ -1,3 +1,11 @@
+"""
+This module provides functionality to convert temperatures from Celsius
+to Fahrenheit and Kelvin.
+functions:
+    - convert_temperature(celsius: float) -> dict[str, float]
+"""
+
+
 def convert_temperature(celsius: float) -> dict[str, float]:
     """
     Convert temperature from Celsius to Fahrenheit and Kelvin.
@@ -10,8 +18,7 @@ def convert_temperature(celsius: float) -> dict[str, float]:
 
     Raises:
         ValueError: If the input is not a valid number or is below absolute zero.
-    functions:
-        - convert_temperature(celsius: float) -> dict[str, float]
+
     Examples:
         >>> convert_temperature(0)
         {'Fahrenheit': 32.0, 'Kelvin': 273.15}

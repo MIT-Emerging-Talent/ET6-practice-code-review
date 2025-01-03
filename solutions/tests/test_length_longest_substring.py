0c6c231b60803d439e5ebbcc010b7d8f7c8921eb
jola-ds <jolajayy@gmail.com>
2025-01-03 16:12:15 +0100

    added test module

---
+++ solutions/tests/test_length_longest_substring.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+This module contains unit tests to validate the functionality of the
+`length_longest_substring` function.
+It checks the length of the longest substring without repeating characters in a string.
+
+Created on: 2025-01-03
+Author: Jola-Moses
+"""
+
+import unittest
+from solutions.length_longest_substring import length_longest_substring
+
+class TestLengthLongestSubstring(unittest.TestCase):
+    """Tests for length_longest_substring"""

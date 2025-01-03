4b335178042e902388eb626bfc77d32f7ed60fdb
reunicorn1 <138371895+reunicorn1@users.noreply.github.com>
2025-01-04 03:14:32 +0400

    Writing the base code for the robot problem

--- solutions/robot.py
+++ solutions/robot.py
@@ -14,22 +14,65 @@ Constraints: The test cases are generated so that the answer will
 be less than or equal to 2 * 109.
 """
 
-def robot(m_: int, n_: int) -> int:
+
+def robot_move(cell, rows, cols, memo) -> int:
+    """
+    This is a helper function that determines the number of unique paths by
+    using dynamic programming and memoization
+
+    Parameters:
+        cell: a tuple made of two elements refers to the current location
+        inside the grid
+        rows: The number of rows in the grid.
+        cols: The number of columns in the grid.
+        memo: a dictionary to save answers for memoization
+
+    Returns -> The number of unique paths from the top-left corner
+    to the bottom-right corner.
+    """
+    if memo.get(cell):
+        return memo.get(cell)
+    if cell[1] >= cols - 1 or cell[0] >= rows - 1:
+        return 1
+    memo[cell] = robot_move((cell[0] + 1, cell[1]), rows, cols, memo) + \
+            robot_move((cell[0], cell[1] + 1), rows, cols, memo)
+    return memo[cell]
+
+
+def robot(rows: int, cols: int) -> int:
     """
     Returns the number of possible unique routes to reach the
     most bottom-right corner
 
     Parameters:
-        m: The number of rows in the grid.
-        n: The number of columns in the grid.
+        rows: The number of rows in the grid.
+        cols: The number of columns in the grid.
 
     Returns -> The number of unique paths from the top-left corner
     to the bottom-right corner.
 
     Raises:
-        AssertionError: if m and n are not integers or less than 1
+        AssertionError: if rows and cols are not integers or less than 1
 
     Examples:
         >>> robot(3, 7)
         28
+        >>> robot(4, 3)
+        10
+        >>> robot(23, 12)
+        193536720
     """
+    # assert the values of rows and clos and make sure they are ints and
+    # greater than 1
+    assert isinstance(rows, int) and rows > 1, "Rows variables has to \
+            be an integer greater than 1"
+    assert isinstance(cols, int) and cols > 1, "Cols variable has to be \
+            greater than 1"
+
+    # use the helper function robot_move to determine the number of paths
+    memo = {}
+    return robot_move((0, 0), rows, cols, memo) # (0, 0) is the initial spot
+
+
+if __name__ == "__main__":
+    print(robot(23, 12))

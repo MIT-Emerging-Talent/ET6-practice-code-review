e4ccc40899571dfc4fe56a7271f2b5349a71ac3b
Abdul Qader Dost <abdulqaderdost@yahoo.com>
2025-01-03 11:55:32 +0100

    Added module dostring for factorial.py file

---
+++ solutions/factorial.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for calculating the factorial of a number.
+
+Module contents:
+    - factorial: Calculate the factorial of a number.
+
+Created on 01-03-2024
+
+@author: Abdul Qader Dost
+"""

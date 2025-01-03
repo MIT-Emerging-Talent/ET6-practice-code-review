acfb8b07fef4ecd9060811b865270acb07b56212
jola-ds <jolajayy@gmail.com>
2025-01-03 15:03:42 +0100

    Added module docstring

---
+++ solutions/length_longest_substring.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A module to find the length of the longest substring in a given string without repeating characters.
+
+Module Contents:
+
+    - length_longest_substring(s: str) -> int:
+    Finds the length of the longest substring without repeating characters.
+
+Created on: 2025-01-03
+Author: Jola-Moses
+"""

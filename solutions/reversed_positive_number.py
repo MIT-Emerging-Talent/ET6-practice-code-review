2e04f617fb4fc394e5d4e4b61df982a191c9a3cb
Raed Eleyan <raedeleyan1@gmail.com>
2025-01-04 13:34:03 +0200

    added some updates to the reversed_positive_number.py file

--- solutions/reversed_positive_number.py
+++ solutions/reversed_positive_number.py
@@ -1,32 +1,26 @@
 """
+A module for reversing the digits of an integer number.
 
-This module provides a function to reverse the digits of an integer.
-
-Author: Raed Eleyan.
-Date: 01/04/2025
+@author: Raed Eleyan.
+@date: 01/04/2025.
 """
 
-def reversed_number(number: int) -> int:
+def reversed_positive_number(number: int) -> int:
     """
-    Reverses the digits of a given non-negative integer.
+    Reverses the digits of a givin positive integer.
 
-    :param number: The integer to be reversed.
-    :return: The reversed integer.
+    :param number: the integer to be reversed. Must be non-negative.
+    :return: the reversed integer.
     :raises ValueError: If the number is negative.
-    Doctests:
-        >>> reversed_number(123)
-        321
-        >>> reversed_number(0)
-        0
-        >>> reversed_number(100)
-        1
-
-    Assumptions:
-    - The input integer is non-negative.
-    - The function removes leading zeros in the reversed number.
+    :raises AssertionError: If the argument isn't a number.
+    >>> reversed_positive_number(123)
+    321
+    >>> reversed_positive_number(0)
+    0
+    >>> reversed_positive_number(100)
+    1
     """
+    assert isinstance(number, int), 'The argument must be an integer.'
     if number < 0:
         raise ValueError('The input integer is negative.')
-    number_to_string = str(number)
-    reverse_string = number_to_string[::-1]
-    return int(reverse_string)
+    return int(str(number)[::-1])

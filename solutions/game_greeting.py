e48ef708634d387491203965a0153c21dedb264f
mxz <amhx19@gmail.com>
2025-01-04 21:10:37 +0200

    adding my code

---
+++ solutions/game_greeting.py
@@ -0,0 +1,32 @@
+"""
+Module: generate a game welcome message
+
+the Module provides a function that generates a welcome message for a player joining a game.
+
+"""
+# the function:
+
+
+def game_greeting(name: str) -> str:
+    """
+    the player inters his name as an input and in return he gets
+    a greeting welcome massage.
+
+
+    parameters:
+      name (string)- The player's name. can be empty string or special characters.
+
+    Returns:
+      string- A formatted Welcome message in the form:
+      "Welcome, [name] to the game"
+
+    Raises:
+      assertionError : if name is not a string
+
+    Example:
+      >>> game_greeting({Mohamed})
+      "Welcome, Mohamed to the game"
+
+    """
+    assert isinstance(name, str)
+    return f"Welcome, {name} to the game"

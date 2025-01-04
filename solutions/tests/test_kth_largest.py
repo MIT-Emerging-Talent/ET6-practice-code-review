2bcb4d9866fb551ed048106ae676e97e2fe0ae21
ahdbasan <ahdbasan2@gmail.com>
2025-01-04 23:01:12 +0300

    Adding find kth largest

--- solutions/tests/test_kth_largest.py
+++ solutions/tests/test_kth_largest.py
@@ -76,19 +76,15 @@ class TestKthLargest(unittest.TestCase):
 
     # WhyIndexError?: there's no element at position 0.
 
-    def test_k_is_negative(self):
-        """It should return the largest element in the list"""
-        actual = kth_largest(["3", "2", "3", "1", "2", "4", "5", "5", "6"], -1)
-        expected = "6"
-        self.assertEqual(actual, expected)
-
-    def test_k_is_zero(self):
-        """It should return the largest element in the list"""
-        actual = kth_largest(["3", "2", "3", "1", "2", "4", "5", "5", "6"], 0)
-        expected = "6"
-        self.assertEqual(actual, expected)
-
     def test_non_numeric_strings(self):
         """It should raise a ValueError if non-numeric strings are included"""
         with self.assertRaises(ValueError):
             kth_largest(["apple", "banana", "cherry"], 2)
+
+    def test_k_is_negative(self):
+        with self.assertRaises(IndexError):
+            kth_largest(["3", "2", "3", "1", "2", "4", "5", "5", "6"], -1)
+
+    def test_k_is_zero(self):
+        with self.assertRaises(IndexError):
+            kth_largest(["3", "2", "3", "1", "2", "4", "5", "5", "6"], 0)

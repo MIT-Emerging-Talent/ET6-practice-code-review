06fd29c450b93561561bec32610d48d9b1d09c3b
Alaa <alaa.algozouli@gmail.com>
2025-01-05 19:31:20 +0200

    made a slight change to pass CI checks

--- solutions/prime_numbers.py
+++ solutions/prime_numbers.py
@@ -43,10 +43,3 @@ def is_prime(N: int) -> bool:
         if N % i == 0:
             return False
         return True
-
-
-number = int(input("Enter a number: "))
-if is_prime(number):
-    print(f"{number} is a prime number.")
-else:
-    print(f"{number} is not a prime number.")

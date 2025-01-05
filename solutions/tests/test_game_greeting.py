0beb468847eeefa0a9955dbb4b4ec299d437e463
Omnia-Agabani <omniaagabani99@gmail.com>
2025-01-05 20:36:14 +0400

    edit the docstring

--- solutions/tests/test_game_greeting.py
+++ solutions/tests/test_game_greeting.py
@@ -11,24 +11,25 @@ from ..game_greeting import game_greeting
 
 
 class TestGameGreeting(unittest.TestCase):
-    # test game greeting function
+    """test game greeting function"""
+
     def test_valid_name(self):
-        # test the function with a valid name
+        """test the function with a valid name"""
         self.assertEqual(game_greeting("Mohamed"), "Welcome, Mohamed to the game")
 
     def test_empty_string(self):
-        # test the function with an empty string
+        """test the function with an empty string"""
         self.assertEqual(game_greeting(""), "Welcome,  to the game")
 
     def test_special_characters(self):
-        # test the function with special characters
+        """test the function with special characters"""
         self.assertEqual(game_greeting("mx@z*"), "Welcome, mx@z* to the game")
 
     def test_integers(self):
-        # test the function with integers
+        """test the function with integers"""
         with self.assertRaises(AssertionError):
             (game_greeting(573))
 
     def test_whitespace_name(self):
-        # test the function with whitespace
+        """test the function with whitespace"""
         self.assertEqual(game_greeting("   "), "Welcome,     to the game")

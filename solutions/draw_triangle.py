ed4f3e30106b0c637ece3bd56ddf7117fdfe65bd
mxz <amhx19@gmail.com>
2025-01-05 19:04:51 +0200

    adding the code

---
+++ solutions/draw_triangle.py
@@ -0,0 +1,49 @@
+"""
+module'draw': it prints a triangle pattern made of stars (*).
+
+the user enters a number and get a triangle of stars in descending order.
+
+"""
+# the function
+
+
+def draw_triangle(n: int) -> None:
+    """
+    Prints a descending triangle pattern of stars (*).
+
+
+    parameters:
+        n (int): The number of stars in the first row.
+        Must be a positive integer.
+
+    Returns:
+        None: The function prints the pattern but does not return any value.
+
+
+    Raises:
+        assertionError: if 'n' is not an integer
+        assertionError: if 'n' is less than one
+
+
+    Examples:
+    >>> draw_triangle(5)
+      *****
+      ****
+      ***
+      **
+      *
+
+      >>> draw_triangle(3)
+        ***
+        **
+        *
+
+    """
+
+    assert isinstance(n, int), " parameter 'n' must be an integer"
+
+    if n < 1:
+        raise ValueError("parameter 'n' must be greater than or equal to 1.")
+
+    for a in range(n, 0, -1):
+        print("*" * a)

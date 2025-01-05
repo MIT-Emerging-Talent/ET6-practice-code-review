165b836c4bea48b8b484ad135c2b226f6133d40a
raneemrami <raneem.ram06@gmail.com>
2025-01-05 12:40:55 +0800

    trying to pass python formatting checks

--- solutions/mean_and_median.py
+++ solutions/mean_and_median.py
@@ -1,27 +1,27 @@
 def mean_and_median(values):
-  """
-  Functions:
-    Get the mean and median of a sorted list of `values`
-
-  Args:
-    values (iterable of float): A list of numbers
-
-  Returns:
-    tuple (float, float): The mean and median
-
-  Raises:
-        TypeError: If `values` is not a list.
     """
-  if not isinstance(values, list):
-    raise TypeError("Input must be a list.")
-  if len(values) == 0:
-    return None, None
-  mean = sum(values) / len(values)
-  values = sorted(values)
-  midpoint = int(len(values) / 2)
-  if len(values) % 2 == 0:
-    median = (values[midpoint - 1] + values[midpoint]) / 2
-  else:
-    median = values[midpoint]
+    Functions:
+      Get the mean and median of a sorted list of `values`
 
-  return mean, median
+    Args:
+      values (iterable of float): A list of numbers
+
+    Returns:
+      tuple (float, float): The mean and median
+
+    Raises:
+          TypeError: If `values` is not a list.
+    """
+    if not isinstance(values, list):
+        raise TypeError("Input must be a list.")
+    if len(values) == 0:
+        return None, None
+    mean = sum(values) / len(values)
+    values = sorted(values)
+    midpoint = int(len(values) / 2)
+    if len(values) % 2 == 0:
+        median = (values[midpoint - 1] + values[midpoint]) / 2
+    else:
+        median = values[midpoint]
+
+    return mean, median

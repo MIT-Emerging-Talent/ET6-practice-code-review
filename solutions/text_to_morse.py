42ab1790bf8e1a2813316cc18182a40ec9c6544b
Ahmed Hussein <ahmed.khalifa11220099@gmail.com>
2025-01-05 17:22:46 +0000

    add solution for Test to Morse code

---
+++ solutions/text_to_morse.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for converting texts into Morse code
+
+Module contents:
+- morse_code: corresponding Morse code for each letter, number and some symbols.
+
+Created on 4 Jan 2025
+@author: Ahmed Hussein
+"""
+
+
+def text_to_morse(text: str) -> str:
+    """Generate the corresponding Morse code of a given text.
+    Letters: A - Z, Numbers: Positive integrs and zero, Symbols: , . ? / - ( )
+
+    Parameters:
+        text: string, the text that needs converting
+
+
+    Returns -> string, the corresponding Morse code
+
+    Raises:
+        AssertionError: if the argument is not a string
+        TypeError: if the character has no corresponding Morse code
+
+    >>> text_to_morse("A")
+    '.-'
+
+    >>> text_to_morse("Hi")
+    '.... ..'
+
+    >>> text_to_morse("forza milan")
+    '..-. --- .-. --.. .- / -- .. .-.. .- -.'
+
+    >>> text_to_morse("CQD DE MGY")
+    '-.-. --.- -.. / -.. . / -- --. -.--'
+
+    >>> text_to_morse("/-)")
+    '-..-. -....- -.--.-'
+
+    """
+
+    assert isinstance(text, str)
+
+    morse_code_dic = {
+        "A": ".-",
+        "B": "-...",
+        "C": "-.-.",
+        "D": "-..",
+        "E": ".",
+        "F": "..-.",
+        "G": "--.",
+        "H": "....",
+        "I": "..",
+        "J": ".---",
+        "K": "-.-",
+        "L": ".-..",
+        "M": "--",
+        "N": "-.",
+        "O": "---",
+        "P": ".--.",
+        "Q": "--.-",
+        "R": ".-.",
+        "S": "...",
+        "T": "-",
+        "U": "..-",
+        "V": "...-",
+        "W": ".--",
+        "X": "-..-",
+        "Y": "-.--",
+        "Z": "--..",
+        "1": ".----",
+        "2": "..---",
+        "3": "...--",
+        "4": "....-",
+        "5": ".....",
+        "6": "-....",
+        "7": "--...",
+        "8": "---..",
+        "9": "----.",
+        "0": "-----",
+        ",": "--..--",
+        ".": ".-.-.-",
+        "?": "..--..",
+        "/": "-..-.",
+        "-": "-....-",
+        "(": "-.--.",
+        ")": "-.--.-",
+        " ": "/",
+    }
+
+    morse_code = []
+
+    # Iterate through each character and give corresponding Morse code
+    for char in text.upper():
+        if char in morse_code_dic:
+            morse_code.append(morse_code_dic[char])
+        else:
+            raise ValueError(f"character: {char} has no corresponding Morse code ")
+
+    return " ".join(morse_code)

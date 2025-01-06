d55058366d3642332cb44d8a4139f989a82654e8
HUDA ALAMASSI <bussiness.hn24@outlook.com>
2025-01-06 03:25:03 +0200

    adding module docstring

---
+++ solutions/largest_perfect_square_less_than_number.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+A module for finding the largest perfect square less than a given number.
+
+Module contents:
+    - largest_perfect_square_less_than_number:
+    returns the largest perfect square that is less than the given number.
+
+
+Created on 2025-01-06
+@author: Huda Alamassi
+"""
---
+++ solutions/tests/test_largest_perfect_square_less_than_number.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Test module for largest_perfect_square_less_than function.
+
+Contains tests for checking the functionality of finding
+
+the largest perfect square less than a given number.
+
+Test categories:
+
+    - Functionality tests: checking if the function correctly identifies
+    the largest perfect square less than a number.
+
+    - Edge tests:
+        - Small positive numbers
+        - Perfect squares
+        - Very large numbers
+        - Zero
+        - Non-integer values
+
+    - Defensive tests: ensuring the function handles invalid inputs such as:
+        - Negative numbers
+        - Non-numeric inputs
+
+
+Created on 2025-01-06
+@author: Huda Alamassi
+"""

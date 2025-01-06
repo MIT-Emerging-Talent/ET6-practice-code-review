338b24b58d9319b4f7372640d62f346c7fe3c472
TagwaHashim <tagwahashim010@gmail.com>
2025-01-06 08:44:14 +0200

    file reformat

--- solutions/Addition.py
+++ solutions/Addition.py
@@ -10,29 +10,31 @@ A module gives a result of adding two numbers.
 
 
 def Addition(num1, num2):
-  """
-  Adds two numbers and returns their sum.
+    """
+    Adds two numbers and returns their sum.
 
-  Args:
-    num1: The first number. Can be an integer or a float.
-    num2: The second number. Can be an integer or a float.
+    Args:
+      num1: The first number. Can be an integer or a float.
+      num2: The second number. Can be an integer or a float.
 
-  Returns:
-    The sum of num1 and num2.
+    Returns:
+      The sum of num1 and num2.
 
-  Raises:
-    TypeError: If either num1 or num2 is not a number (int or float).
+    Raises:
+      TypeError: If either num1 or num2 is not a number (int or float).
 
-  Examples:
-    >>> Addition(3, 5)
-    8
-    >>> Addition(2.5, 3.7)
-    6.2
-    >>> Addition("hello", 2.718) 
-    Traceback (most recent call last):
-            ...
-    AssertionError: Both arguments must be numbers (int or float).
-  """
-  assert (isinstance(num1, (int, float)) and isinstance(num2, (int, float))), "Both arguments must be numbers (int or float)."
-     
-  return num1 + num2
+    Examples:
+      >>> Addition(3, 5)
+      8
+      >>> Addition(2.5, 3.7)
+      6.2
+      >>> Addition("hello", 2.718)
+      Traceback (most recent call last):
+              ...
+      AssertionError: Both arguments must be numbers (int or float).
+    """
+    assert isinstance(num1, (int, float)) and isinstance(
+        num2, (int, float)
+    ), "Both arguments must be numbers (int or float)."
+
+    return num1 + num2

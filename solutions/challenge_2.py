f8257ab31154c85bea78559360666d42da481b6d
Bekuretsion222@gmail.com <bekuretsion222@gmail.com>
2025-01-06 14:00:53 +0100

    Reformat Python files to adhere to style guidelines

--- solutions/challenge_2.py
+++ solutions/challenge_2.py
@@ -1,6 +1,6 @@
 """
 
-Write a function that takes a list of numbers 
+Write a function that takes a list of numbers
 and returns a dictionary with sums of the even and odd numbers in the list.
 
 """

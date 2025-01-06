a5646120f06b94790a24dfc958806889a991d28a
ayhm01 <ayhamalser2005@gmail.com>
2025-01-06 13:22:24 +0200

    added factorial_calculator

---
+++ solutions/factorial_calculator.py
@@ -0,0 +1,39 @@
+"""factorial calculator"""
+
+
+def factorial(n: int) -> int:
+    """
+    Parameters:
+    n(int): The number to calculate the factorial of. it should be a positive integer only
+
+    Returns:
+    int: the factorial of the given number
+
+    Raises:
+    ValueError -> If the input is None or a Negative integer
+    TypeError -> if the input is not an integer
+
+    Examples:
+
+    >>> factorial(5)
+    120
+    >>> factorial(0)
+    1
+    >>> factorial(3)
+    6
+
+    """
+
+    if n is None:
+        raise ValueError("Input cannot be None")
+
+    if not isinstance(n, int):
+        raise TypeError("Input must be an integer")
+
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers")
+
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact

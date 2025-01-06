7f23ddd7dedd5e33ddc92e94c37d7d7659a17a2f
Taher <tahirsaban2022@gmail.com>
2025-01-06 23:30:39 +0300

    Fix formatting issues

--- solutions/tests/Test_convert_decimal_to_binary.py
+++ solutions/tests/Test_convert_decimal_to_binary.py
@@ -10,11 +10,15 @@ Created on 01/01/2025
 import unittest
 
 from Convert_Decimal_To_Binary import conv_dec_bin
+
+
 class TestConvertDecimalToBinary(unittest.TestCase):
     """Test the conv_dec_bin function"""
+
     def test_Decimal_num(self):
         """It should evaluate 6 to 110"""
-        self.assertEqual(conv_dec_bin(6),"110")
+        self.assertEqual(conv_dec_bin(6), "110")
+
     def test_non_Decimal_num(self):
         """It should raise an assertion error if the argument is not a decimal number"""
         with self.assertRaises(AssertionError):

4204a33d1fcd0170eca7028dc548bdedae711bf9
lanamusienko <lanamus88@gmail.com>
2025-01-06 22:36:04 -0800

    Add fizzbuz_challenge

--- solutions/fizzbuzz_challenge.py
+++ solutions/fizzbuzz_challenge.py
@@ -1,33 +1,9 @@
-# FizzBuzz Challenge
 """
-Module resolving FizzBuzz problem with function tha takes an integer n as input
-and prints the numbers from 1 to n with the rules:
-- if a number is divisible by 3, print 'Fizz' instead of the number;
-- if a number is divisible by 5, print 'Buzz' instead of the number;
-- if a number is divisible by both 3 and 5, print 'FizzBuzz';
-- otherwise, print the number itself.
+Module resolving FizzBuzz problem according to given rules.
 
-Parameters: integer number
-
-Result: print numbers from 1 to n with above rules.
-
-Example:
->>> fizzbuzz(15)
-1
-2
-Fizz
-4
-Buzz
-Fizz
-7
-8
-Fizz
-Buzz
-11
-Fizz
-13
-14
-FizzBuzz
+Module contents:
+    - fizzbuzz: take integer n and prints numbers from 1 to n
+        with the rules.
 
 Created: 01/04/2025
 Author: Svitlana Musiienko
@@ -36,24 +12,76 @@ Author: Svitlana Musiienko
 
 
 def fizzbuzz(n):
+    """
+    Fizzbuz function takes an integer n as input and prints the numbers
+    from 1 to n with the rules:
+        - if a number is divisible by 3, print 'Fizz' instead of the number;
+        - if a number is divisible by 5, print 'Buzz' instead of the number;
+        - if a number is divisible by both 3 and 5, print 'FizzBuzz';
+        - otherwise, print the number itself.
+
+    Parameters: integer number (n)
+
+    Returns: print numbers from 1 to n based on given rules.
+
+    Raises:
+        AssertionError: if the argument is not integer
+        AssertionError: if the argument is less than 0
+
+    Example:
+
+    >>>> fizzbuzz (0)
+    []
+
+    >>>> fizzbuzz (5)
+    1
+    2
+    Fizz
+    4
+    Buzz
+
+    >>>> fizzbuzz(15)
+    1
+    2
+    Fizz
+    4
+    Buzz
+    Fizz
+    7
+    8
+    Fizz
+    Buzz
+    11
+    Fizz
+    13
+    14
+    FizzBuzz
+
+    """
+    # the number should be an integer greater than 0
+    assert isinstance(n, int)
+    # the number is less than 0
+    assert n >= 0
+    
+    result = []
     # loop through numbers from 1 to n
     for num in range(1, n + 1):
         # check if the number is divisible by both 3 and 5
         if num % 3 == 0 and num % 5 == 0:
             # print "FizzBuzz" for multiples of both 3 and 5
-            print("FizzBuzz")
+            result.append("FizzBuzz")
         # check if the number is divisible by 3 only
         elif num % 3 == 0:
             # print "Fizz" for multiples of 3
-            print("Fizz")
+            result.append("Fizz")
         # check if the number is divisible by 5 only
         elif num % 5 == 0:
             # print "Buzz" for multiples of 5
-            print("Buzz")
+            result.append("Buzz")
         # print the number if none of the above conditions are met
         else:
-            print(num)
-
-
-# run function with example n=15
-fizzbuzz(15)
+            result.append(num)
+    
+    
+    return result
+    

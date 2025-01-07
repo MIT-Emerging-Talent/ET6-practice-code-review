c61efe4afaee27212673a5ba61ce3567d3b21e60
Rumiya <ismatova.rumiya@gmail.com>
2025-01-06 22:22:06 -0600

    Added doc tests for the function

--- solutions/sum_elements.py
+++ solutions/sum_elements.py
@@ -1,13 +1,40 @@
-def sum_elements(numbers):
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for calculating sum of integers in a given list.
+
+Module contents:
+    - sum_elements: A function to calculate the sum of integers in a list. 
+
+Created on 2025-01-06
+@author: Rumiya Ismatova
+"""
+
+def sum_elements(numbers:list)->int:
     """
     Calculate the sum of all elements in a list.
 
     Args:
-        numbers (list): A list of integers or floats.
+        numbers[int]: A list of integers
+        
+    Returns: 
+        int: the sum of the numbers in the list
+        
+    >>> sum_elements([1,2,3])
+    6
 
-    Returns:
-        int or float: The sum of the list's elements.
-                      Returns 0 for an empty list.
+    >>> sum_elements([2,3,3])
+    8
+
+    >>> sum_elements([])
+    0
     """
+    if len(numbers)==0:
+        return 0
+    
+
+    
     return sum(numbers)
+
+
 

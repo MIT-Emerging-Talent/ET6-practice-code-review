e4c91d023d5c103581f9b907dfbe02681a043ece
Shadi Shadab <46791602+ShadiShadab@users.noreply.github.com>
2025-01-09 22:57:40 +0200

    Update Find-Prime-Numbers.py

--- solutions/Find-Prime-Numbers.py
+++ solutions/Find-Prime-Numbers.py
@@ -4,8 +4,8 @@ r"""
 This module defines the function `find_primes_up_to_n` to identify all prime numbers 
 up to a given integer \( N \). A prime number is greater than 1 and divisible only by 1 and itself.
 
-Author: Zeinab Shadabshoar
 Date: 09 01 2025
+Author: Zeinab Shadabshoar
 """
 
 

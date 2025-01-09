8710d192d6944addb1554928d09e05bb787ef729
Shadi Shadab <46791602+ShadiShadab@users.noreply.github.com>
2025-01-09 16:18:01 +0200

    Update __init__.py

--- solutions/__init__.py
+++ solutions/__init__.py
@@ -1,5 +1,5 @@
 """
-##Challenge Overview
+##Challenge Overview: 
 The goal of this challenge is to count how many prime numbers are present in a given list of integers. A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 
 ### Steps to Solve

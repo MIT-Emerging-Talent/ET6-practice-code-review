0917209e06514d644563ac4eea748b86bb1c88c4
Tamara Saqer <btamarasaqer@gmail.com>
2025-01-09 15:14:49 +0200

    Update_please work_test_is_odd_or_even.py

--- solutions/tests/test_is_odd_or_even.py
+++ solutions/tests/test_is_odd_or_even.py
@@ -1,4 +1,9 @@
 import unittest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
 from solutions.is_odd_or_even import is_odd_or_even
 
 

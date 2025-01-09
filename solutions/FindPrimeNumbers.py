adf66e98ac84c0e7300eb6fa3874d3d50eee1e75
Shadi Shadab <46791602+ShadiShadab@users.noreply.github.com>
2025-01-09 18:01:30 +0200

    Update FindPrimeNumbers.py

--- solutions/FindPrimeNumbers.py
+++ solutions/FindPrimeNumbers.py
@@ -12,18 +12,6 @@ Identify all prime numbers up to a given integer \( N \). A prime number is grea
 3. **Output**:
    - List of primes up to \( N \), e.g., [2, 3, 5, 7, 11, 13, 17, 19].
 
-### Implementation
-1. Iterate Through Numbers:
-   - Loop from 2 to \( N \).
-
-2. Check if Each Number is Prime:
-   - Function to check if a number is prime:
-     - Return False if < 2.
-     - Check divisibility from 2 to sqrt(number).
-     - Return True if no divisors found.
-
-3. Collect Prime Numbers:
-   - Use the function to collect primes up to \( N \).
 
 ----------------------------------------------------------------------------------------------------------------------------------------
 """"

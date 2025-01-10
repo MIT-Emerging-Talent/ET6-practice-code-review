8a3763f4260b91f4e20dfcd327cf8943515b25fb
yoolmalaak <yoolmalaak@gmail.com>
2025-01-10 12:00:55 +0000

    updated

--- solution/verify_data.py
+++ solution/verify_data.py
@@ -1,4 +1,6 @@
-"""This function validate names,email address, password, credit card number, and id number"""
+"""
+test for verification function. the test validate the data that is passed the criteria, and vice versa.
+"""
 
 
 def validate_name(name):
@@ -9,6 +11,13 @@ def validate_name(name):
 
     Returns:
         bool: true if the name is valid, false otherwise.
+    examples:
+    >>> validate_name("Camilla Massa")
+    True
+    >>> validate_name("Joe_Biden")
+    False
+    >>> validate_name("Mohammed122")
+    False
     """
     if all(char.isalpha() or char.isspace() for char in name):
         return True
@@ -24,29 +33,20 @@ def validate_id_number(id_number):
 
     Returns:
         bool:  true if true, otherwise false
+    examples:
+    >>> validate_id_number("204456247")
+    True
+    >>> validate_id_number("204456")
+    False
+    >>> validate_id_number("2A4576983021765")
+    False
+    >>> validate_id_number("24-6 7684 9085")
+    False
     """
     if id_number.isdigit() and len(id_number) >= 9:
         return True
     else:
         return False
-
-
-def validate_email(email):
-    """Validate the email address that contain @ and dot(.)
-
-    parameters:
-        email (str): email address to be validated
-
-    Returns:
-        bool: true if email address is valid, false otherwise
-    """
-    if "@" not in email or "." not in email:
-        return False
-    at_index = email.index("@")
-    dot_index = email.rindex(".")
-    if at_index == 0 or dot_index < at_index + 2 or dot_index == len(email) - 1:
-        return False
-    return True
 
 
 def validate_credit_card(card_number):
@@ -57,6 +57,17 @@ def validate_credit_card(card_number):
 
     Returns:
         bool: true if the credit card number is valid, otherwise false
+    examples:
+    >>> validate_credit_card("6011 1111 1111 1117")
+    True
+    >>> validate_credit_card("3782 8224 6310 005")
+    True
+    >>> validate_credit_card("12345-6789038693")
+    False
+    >>> validate_credit_card("2A89497534975jc")
+    False
+    >>> validate_credit_card("abcdefghijkl")
+    False
     """
     card_number = card_number.replace(" ", "")
     if not card_number.isdigit():
@@ -73,6 +84,35 @@ def validate_credit_card(card_number):
     return total % 10 == 0
 
 
+def validate_email(email):
+    """Validate the email address that contain @ and dot(.)
+
+    parameters:
+        email (str): email address to be validated
+
+    Returns:
+        bool: true if email address is valid, false otherwise
+    examples:
+    >>> validate_email("martha@gmail.com")
+    True
+    >>> validate_email("katrina@school.edu")
+    True
+    >>> validate_email("patrick-email.com")
+    False
+    >>> validate_email("adam@gmail_com")
+    False
+    >>> validate_email("ahmed@")
+    False
+    """
+    if "@" not in email or "." not in email:
+        return False
+    at_index = email.index("@")
+    dot_index = email.rindex(".")
+    if at_index == 0 or dot_index < at_index + 2 or dot_index == len(email) - 1:
+        return False
+    return True
+
+
 def validate_password(password):
     """validate password base on the following criteria
     - at least 8 characters
@@ -86,6 +126,19 @@ def validate_password(password):
 
     Returns:
         bool: true if password is valid, false otherwise.
+    examples:
+    >>> validate_password("Johneavans@2026.com")
+    True
+    >>> validate_password("J@c0b.israel")
+    True
+    >>> validate_password("johnevans@2028.com")
+    False
+    >>> validate_password("Megan_mit$%")
+    False
+    >>> validate_password("202987423690")
+    False
+    >>> validate_password("Short2!")
+    False
     """
     if len(password) < 8:
         return False

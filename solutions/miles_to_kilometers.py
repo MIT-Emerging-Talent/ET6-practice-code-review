2c0505506bb2091c72d5c1d0651ab32131f393d0
ObayCipher <obey19955@gmail.com>
2025-01-10 22:12:12 +0100

    add o mile o.o kilometers

--- solutions/miles_to_kilometers.py
+++ solutions/miles_to_kilometers.py
@@ -6,10 +6,15 @@ the conversion factor: 1 mile = 1.609344 kilometers. It also validates that the 
 is a valid, non-negative number.
 
 Example usage:
+    >>> miles_to_kilometers(0)
+    0.000
+
     >>> miles_to_kilometers(1)
     1.609344
+
     >>> miles_to_kilometers(10)
     16.09344
+
 """
 
 

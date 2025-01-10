24ac3931837568bebe40bf102290a37cec56390d
ObayCipher <obey19955@gmail.com>
2025-01-10 21:53:27 +0100

    add docstrings

---
+++ solutions/miles_to_kilometers.py
@@ -0,0 +1,13 @@
+"""
+Convert miles to kilometers.
+
+This module provides a function to convert distances from miles to kilometers using
+the conversion factor: 1 mile = 1.609344 kilometers. It also validates that the input
+is a valid, non-negative number.
+
+Example usage:
+    >>> miles_to_kilometers(1)
+    1.609344
+    >>> miles_to_kilometers(10)
+    16.09344
+"""

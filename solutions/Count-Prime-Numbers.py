7e34e707dd11a8489a484f848837bb6359622f8b
Shadi Shadab <46791602+ShadiShadab@users.noreply.github.com>
2025-01-10 13:20:20 +0200

    Update Count-Prime-Numbers.py

--- solutions/Count-Prime-Numbers.py
+++ solutions/Count-Prime-Numbers.py
@@ -11,6 +11,7 @@ Date: 09 01 2025
 Author: Zeinab Shadabshoar
 """
 
+
 def is_prime(n: int) -> bool:
     """
     Checks if a given number is prime.
@@ -35,10 +36,11 @@ def is_prime(n: int) -> bool:
 
     if n <= 1:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             return False
     return True
+
 
 def count_primes(numbers: list) -> int:
     """
@@ -68,6 +70,7 @@ def count_primes(numbers: list) -> int:
 
     return prime_count
 
+
 # Example usage
 numbers = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
-print("Number of prime numbers:", count_primes(numbers)) 
+print("Number of prime numbers:", count_primes(numbers))

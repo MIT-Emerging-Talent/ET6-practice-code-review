a01c9639f0c1a5dfb6543d3a9351d536793d7080
Ahd Basan <ahdbasan2@gmail.com>
2025-01-11 01:22:37 +0300

    06_meeting_minutes.
    Last Meeting Minutes

---
+++ notes/meeting_minutes/06_meeting_minutes.m
@@ -0,0 +1,22 @@
+
+# **Meeting 06**
+
+**11/01/2025  
+12.30 EST - 2.30 EST  
+Google Meet  
+Attendees: 6**
+
+## Agenda
+
+Retrospective and Group Checklist
+
+### **Topics Discussed:**
+
+**Group Checklist**
+
++ Delete Branches 
++ Did the Group Checklist 
+Tracking all on the [Discussions](https://github.com/MIT-Emerging-Talent/ET6-foundations-group-28/discussions)
+<!--markdownlint-disable MD034 MD013-->
+
+

326c41a3fd43276339477cf3db8aac4cfbe98840
Cyne Jarvis J. Zarceno <24103284@usc.edu.ph>
2025-01-10 11:14:52 +0800

    Removed lines 25,26, and 27

--- solutions/tests/test_volume_of_the_sphere.py
+++ solutions/tests/test_volume_of_the_sphere.py
@@ -22,9 +22,6 @@ from solutions.volume_of_the_sphere import volume_of_the_sphere
 
 # Add parent directory to Python path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
 
 
 class TestVolumeOfTheSphere(unittest.TestCase):

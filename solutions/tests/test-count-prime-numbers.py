372ab97382c0906ff3451a7630e773d604fb57c5
Shadi Shadab <46791602+ShadiShadab@users.noreply.github.com>
2025-01-10 13:21:35 +0200

    Update test-count-prime-numbers.py

--- solutions/tests/test-count-prime-numbers.py
+++ solutions/tests/test-count-prime-numbers.py
@@ -90,5 +90,6 @@ class TestPrimeFunctions(unittest.TestCase):
         with self.assertRaises(AssertionError):
             count_primes([2, 3, "four", 5])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()

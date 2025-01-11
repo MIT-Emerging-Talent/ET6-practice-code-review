002c4b3de6e2c56a74079d3e501c7fa69691e844
salemAmassi <salem.amassi@gmail.com>
2025-01-11 13:57:08 +0200

    Fix formatting issue

--- solutions/tests/test_sum_digits.py
+++ solutions/tests/test_sum_digits.py
@@ -11,7 +11,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
-from solutions.sum_digits import sum_digits  
+from solutions.sum_digits import sum_digits
 
 
 class TestSumDigits(unittest.TestCase):

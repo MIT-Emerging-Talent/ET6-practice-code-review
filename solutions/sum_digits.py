b0b9cd8f05a766e2a5d00c2429ce60ad0ca36edb
salemAmassi <salem.amassi@gmail.com>
2025-01-11 13:58:55 +0200

    Modify arg name

--- solutions/sum_digits.py
+++ solutions/sum_digits.py
@@ -6,12 +6,12 @@ It uses recursion to solve the sum of digits of n
 """
 
 
-def sum_digits(n):
+def sum_digits(num: int):
     """
     Calculate the sum of the digits of a given non-negative integer using recursion.
 
     Args:
-    n (int): A non-negative integer whose digits will be summed.
+    num (int): A non-negative integer whose digits will be summed.
 
     Returns:
     int: The sum of the digits of the given integer.
@@ -27,14 +27,14 @@ def sum_digits(n):
     15
     """
 
-    validate_input(n)
+    validate_input(num)
 
-    if n == 0:
+    if num == 0:
         return 0
     else:
-        return n % 10 + sum_digits(n // 10)
+        return num % 10 + sum_digits(num // 10)
 
 
-def validate_input(n):
-    if not isinstance(n, int) or n < 0:
+def validate_input(num):
+    if not isinstance(num, int) or num < 0:
         raise ValueError("Input must be a non-negative integer.")

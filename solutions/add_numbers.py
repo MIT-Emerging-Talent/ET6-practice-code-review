bad4322c30a02d85d58ccd2032f177086e976a71
Momtaz <m.yaqubi333@gmail.com>
2025-01-11 19:36:31 +0430

    solution added

---
+++ solutions/add_numbers.py
@@ -0,0 +1,17 @@
+def add_numbers(a, b):
+    """
+    Adds two numbers together.
+
+    Parameters:
+        a (int, float): The first number to add.
+        b (int, float): The second number to add.
+
+    Returns:
+        int, float: The result of adding `a` and `b`.
+
+    Raises:
+        TypeError: If either `a` or `b` is not a number.
+    """
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Both arguments must be numbers")
+    return a + b

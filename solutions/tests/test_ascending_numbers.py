6e5b02af682a05830785c24518907a685c59591f
Osei Agyemang Sarfo <oseiagyemangsarfo@gmail.com>
2025-01-12 13:02:47 +0000

    Update test_ascending_numbers.py
    Updated test

--- solutions/tests/test_ascending_numbers.py
+++ solutions/tests/test_ascending_numbers.py
@@ -15,7 +15,7 @@ from ..ascending_numbers import ascending_numbers
 class TestAscendingNumbers(unittest.TestCase):
     """Test the ascending_numbers function"""
 
-    def test1(self):
+    def ascending_integers(self):
         "It should evaluate [56,78, 45, 22] to [22, 45, 56, 78]"
         actual = ascending_numbers(
             [56, 78, 45, 22]
@@ -23,7 +23,7 @@ class TestAscendingNumbers(unittest.TestCase):
         expected = [22, 45, 56, 78]  # hand-written answer.
         self.assertEqual(actual, expected)
 
-    def test2(self):
+    def ascending_floats(self):
         "It should evaluate [27.8,34.9,67.9,22.1] to [22.1, 27.8, 34.9, 67.9]"
         actual = ascending_numbers(
             [27.8, 34.9, 67.9, 22.1]

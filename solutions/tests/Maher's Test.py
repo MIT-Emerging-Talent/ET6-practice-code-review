54ba289247ddb3dff5bd45f09bc8a4c6638bc709
Muhannad-Assaf <133141191+MuhannadGTR@users.noreply.github.com>
2025-01-12 14:10:34 +0200

    Update Maher's Test.py

--- solutions/tests/Maher's Test.py
+++ solutions/tests/Maher's Test.py
@@ -2,7 +2,7 @@ import unittest
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
+
 
 # Assuming the module is named housing_analysis and the functions are imported
 from housing_analysis import (

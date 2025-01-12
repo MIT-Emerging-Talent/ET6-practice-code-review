6058f756255e7b60caa7c464119aa98646b91b80
Vahablotfi <vahablotfi1@gmail.com>
2025-01-11 20:37:00 -0700

    unit test for check_coprime added

---
+++ solutions/tests/test_check_coprime.py
@@ -0,0 +1,9 @@
+"""Tests for check_coprime function.
+
+
+This module contains unit tests to ensure the check_coprime function works as expected,
+including handling edge cases and defensive assertions.
+
+@author: Vahab
+@created: 11/01/2025
+"""

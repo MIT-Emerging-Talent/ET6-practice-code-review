40649497fe0a057ea103f99388e9d0c1fde5401a
muqaddas96 <muqaddas.rehman@sensys.com>
2025-01-12 22:08:31 +0300

    fixed classes

--- solutions/tests/test_find_largest_number.py
+++ solutions/tests/test_find_largest_number.py
@@ -12,18 +12,18 @@ from solutions.find_largest_number import largest_number
 
 
 class TestLargestNumber(unittest.TestCase):
-    def test_largest(self):
+    def test_largest_positive(self):
         numbers = [1, 2, 3, 4, 21, 12, 8]
         result = largest_number(numbers)
         self.assertEqual(result, 21)
 
-    def test_largest(self):
-        numbers = [1, 2, 3, 4, 21, 42, 8]
+    def test_largest_negative(self):
+        numbers = [-1, -2, -3, -4, -21, -42, -8]
         result = largest_number(numbers)
-        self.assertEqual(result, 42)
+        self.assertEqual(result, -1)
 
-    def test_largest(self):
-        numbers = [1, 2, 3, 4, 79, 12, 8]
+    def test_largest_mix(self):
+        numbers = [-1, 2, -3, 4, 79, 12, 8]
         result = largest_number(numbers)
         self.assertEqual(result, 79)
 
